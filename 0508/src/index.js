09db82ab803494fe24d41228ecb0a76c1dc869ee
jasonbanboa <jasonbanboa123@gmail.com>
2023-05-09 09:23:21 +0900

    changed file name

--- 0508/src/index.js
+++ 0508/src/index.js
@@ -1,5 +1,6 @@
-import ReactDOM from 'react-dom/client';
-import Router from './pages/router';
+import ReactDOM from 'react-dom/client'
+import Router from './pages/Router'
+import './reset.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Router />);
--- 0508/src/pages/router.jsx
+++ 0508/src/pages/router.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Main from '../Main';
+import Main from './main/index';
 
 export default function Router() {
   return (
