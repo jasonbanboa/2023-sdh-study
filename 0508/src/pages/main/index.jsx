1b84d6c1a944bda4a377d9527045e755e4bcb36c
jasonbanboa <jasonbanboa123@gmail.com>
2023-05-09 10:22:48 +0900

    feat todo page

--- 0508/src/pages/main/index.jsx
+++ 0508/src/pages/main/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Button from '../../components/Button'
 import * as S from './styled'
+import { Link } from 'react-router-dom'
+
 
 export default function Main() {
   return (
     <S.Container>
       <S.MainWrapper>
         <div>메인페이지</div>
-        <Button>만들기</Button>
+        <Button><Link to="/todos">만들기</Link></Button>
       </S.MainWrapper>
     </S.Container>
   )
--- 0508/src/pages/router.jsx
+++ 0508/src/pages/router.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Main from './main/index';
+import Todos from './todos';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />}/>
+        <Route path="/todos" element={<Todos />}/>
       </Routes>
     </BrowserRouter>
   );
