e52004dd5df440c35a9cb6e524538b1d43ec058e
jasonbanboa <jasonbanboa123@gmail.com>
2023-05-10 11:21:33 +0900

    style todo

--- 0508/src/components/Button/styled.js
+++ 0508/src/components/Button/styled.js
@@ -2,8 +2,15 @@
 import styled from 'styled-components'
 
 export const Button = styled.button`
-  display: flex;
+  white-space: nowrap;
   align-items: center;
   justify-content: center;
   border-radius: 4px;
+  background-color: transparent;
+  padding: 5px 10px;
+  border: 1px solid #eee;
+  cursor: pointer;
+  &:disabled {
+    cursor: not-allowed;
+  }
 `;
--- 0508/src/pages/todos/ItemList/styled.js
+++ 0508/src/pages/todos/ItemList/styled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const ItemList = styled.div`
   display: flex;
+  width: 100%;
   flex-direction: column;
   gap: 8px;
 `;
