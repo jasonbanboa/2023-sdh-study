0050b6bdf66ac290a4f366c6fa9713d3e07ea193
jasonbanboa <jasonbanboa123@gmail.com>
2023-05-11 09:59:21 +0900

    removed button

--- 0508/src/components/Todo/index.jsx
+++ 0508/src/components/Todo/index.jsx
@@ -5,7 +5,6 @@ export default function Todo({ children, deleteTodo }) {
   return (
     <S.Container>
       <S.Title>{children}</S.Title>
-      <Button>중요</Button>
       <Button onClick={deleteTodo}>삭제</Button>
     </S.Container>
   );
